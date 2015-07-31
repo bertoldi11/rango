df76408089bdbd026338048d9616dc54df96420b
Vinícius Bertoldi <bertoldi11@gmail.com>
2015-07-31 18:00:50 -0300

    Finalizando Capítulo 4 - Django Basics

--- rango/views.py
+++ rango/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse('Rango diz Ola <a href="/rango/about">About</a>')
+
+
+def about(request):
+    return HttpResponse('Rango diz: Aqui e a pagina Sobre <a href="/rango">Index</a>')
